0a2105e8bee786d6b72931cbd952b35a504fe1f6
JuanLara <sebas981@live.com>
2023-03-26 23:56:59 -0300

    Update completado

--- src/api/apiEmployee.js
+++ src/api/apiEmployee.js
@@ -5,37 +5,56 @@ const url = "http://localhost:5000/api/employee";
 export const getEmployees = createAsyncThunk(
   "employee/getEmpleado",
   async (page) => {
-    const response = await fetch(`${url}?page=${page}`);
-    const responseData = await response.json();
-    return responseData.data;
+    try {
+      const response = await fetch(`${url}?page=${page}`);
+      const responseData = await response.json();
+      return responseData.data;
+    } catch (error) {
+      console.log(error)
+    }
   }
 );
 
 export const updateEmployed = createAsyncThunk (
   "employee/updateEmpleado",
-  async(id, employee) =>{
-    const response = await fetch(`${url}/${id}`,{
-      method: "PUT",
-      headers:{
-        "Content-Type":"application/json"
-      },
-      body: JSON.stringify(employee)
-    });
-    const responseData = await response.json();
-    return responseData;
+  async(employee) =>{
+    try {
+      const response = await fetch(`${url}/${employee.idemployee}`,{
+        method: "PUT",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+          first_name: employee.first_name,
+          last_name: employee.last_name,
+          cuit: employee.cuit,
+          team_id: employee.team_id,
+          join_date: employee.join_date,
+          rol: employee.rol,
+        }),
+      });
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      console.log(error)
+    }
   }
 )
 
 export const deleteEmployee = createAsyncThunk(
   "employee/deleteEmployee",
   async (id) => {
-    await fetch(`${url}/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return id;
+    try {
+      await fetch(`${url}/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return id;
+    } catch (error) {
+      console.log(error)
+    }
   }
 );
 
@@ -43,27 +62,36 @@ export const createEmployee = createAsyncThunk(
   "employee/createEmployee",
   async (employee) => {
     const { first_name, last_name, cuit, team_id, join_date, rol } = employee;
-    const response = await fetch(`${url}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        first_name,
-        last_name,
-        cuit,
-        team_id,
-        join_date,
-        rol,
-      }),
-    });
-    const responseData = await response.json();
-    return responseData;
+    try {
+      const response = await fetch(`${url}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name,
+          last_name,
+          cuit,
+          team_id,
+          join_date,
+          rol,
+        }),
+      });
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      console.log(error)
+    }
   }
 );
 
 export const getEmployeeById = async (id) => {
-  const response = await fetch(`${url}/${id}`);
-  const responseData = await response.json();
-  return responseData.menssage;
+  try {
+    const response = await fetch(`${url}/${id}`);
+    const responseData = await response.json();
+    return responseData.menssage;
+  } catch (error) {
+    console.log(error)
+  }
+
 };
--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -9,12 +9,8 @@ const Pagination = ({numPages, onGetPage , currentPage}) => {
   return (
     <div className="col mt-4">
       <ul className="pagination justify-content-center">
-        <li className="page-item disabled">
-          <button className="page-link">Anterior</button>
-        </li>
         {pageNumbers.map((pageValue) => {
           return (
-            <>
               <li key={pageValue} className={`page-item ${pageValue === currentPage ? 'active': ""}`}>
                 <button className="page-link"
                 onClick={(e)=> onGetPage(e,pageValue)}
@@ -22,12 +18,8 @@ const Pagination = ({numPages, onGetPage , currentPage}) => {
                   {pageValue}
                 </button>
               </li>
-            </>
           );
         })}
-        <li className="page-item">
-          <button className="page-link">Siguiente</button>
-        </li>
       </ul>
     </div>
   );
